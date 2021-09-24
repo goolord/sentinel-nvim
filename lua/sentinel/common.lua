9f16aa7d6ae22ef287bff9cd5a636081b0f5d3e0
Zachary Churchill <zacharyachurchill@gmail.com>
2021-09-24 11:21:07 -0400

    organizing

--- lua/sentinel/common.lua
+++ lua/sentinel/common.lua
@@ -7,4 +7,23 @@ function M.register_provider(opts)
     _G.sentinel.providers[opts.req.name] = opts
 end
 
+-- Function to retrieve console output
+function os.capture(cmd, raw)
+  local f = assert(io.popen(table.concat(cmd, ' '), 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  if raw then return s end
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  return s
+end
+
+function M.unlines(str)
+    local lines = {}
+    for s in str:gmatch("[^\r\n]+") do
+        table.insert(lines, s)
+    end
+    return lines
+end
+
 return M
--- lua/sentinel/providers/files.lua
+++ lua/sentinel/providers/files.lua
@@ -1,23 +1,6 @@
----
--- Function to retrieve console output
---
-function os.capture(cmd, raw)
-  local f = assert(io.popen(table.concat(cmd, ' '), 'r'))
-  local s = assert(f:read('*a'))
-  f:close()
-  if raw then return s end
-  s = string.gsub(s, '^%s+', '')
-  s = string.gsub(s, '%s+$', '')
-  return s
-end
+local common = require'sentinel.common'
 
-local function unlines(str)
-    local lines = {}
-    for s in str:gmatch("[^\r\n]+") do
-        table.insert(lines, s)
-    end
-    return lines
-end
+local input = ''
 
 local function button(filename)
     local opts = {
@@ -39,11 +22,14 @@ local function button(filename)
     }
 end
 
-local find_command = { "fd", "--type", "f", "--hidden", "--no-ignore", "-L" }
+local find_command = { "fd", "--type", "f", "-L" }
 
-local function find()
+local function find(search_str)
     local buttons = {}
-    for _, file in ipairs(unlines(os.capture(find_command, false))) do
+    local find_query = {}
+    vim.list_extend(find_query, find_command)
+    vim.list_extend(find_query, {search_str})
+    for _, file in ipairs(common.unlines(os.capture(find_query, false))) do
         local btn = button(file)
         table.insert(buttons, btn)
     end
@@ -52,7 +38,7 @@ end
 
 require'sentinel.common'.register_provider {
     layout = {
-        { type = "group", val = find }
+        { type = "group", val = find(input) }
     },
     req = {
         name = 'files',
